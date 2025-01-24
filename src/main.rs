85b61b523742d195311b92cce0090fb1eb8e7f1c
lkaubs <221017050@aluno.unb.br>
2025-01-24 17:03:30 -0300

    Cargo fmt and add super use

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::io;
 use crate::repl::repl::repl;
+use std::io;
 
 pub mod interpreter;
 pub mod ir;
 pub mod parser;
-pub mod tc;
 pub mod repl;
+pub mod tc;
 
 fn main() -> io::Result<()> {
     repl()
--- src/repl/repl.rs
+++ src/repl/repl.rs
@@ -1,9 +1,9 @@
-use std::io::{self, Write};
-use std::collections::HashMap;
-use crate::interpreter::interpreter::execute;
-use crate::parser::parser::*;
 use crate::interpreter::interpreter::eval;
+use crate::interpreter::interpreter::execute;
 use crate::ir::ast::Expression;
+use crate::parser::parser::*;
+use std::collections::HashMap;
+use std::io::{self, Write};
 use std::process::Command;
 
 pub fn repl() -> io::Result<()> {
@@ -41,34 +41,32 @@ pub fn repl() -> io::Result<()> {
         if input == "" {
             continue;
         }
-        
+
         // Reset the output
-        let mut output: Result::<String, String> = Ok(format!(""));
+        let mut output: Result<String, String> = Ok(format!(""));
 
         // Parsing of expressions
         match expression(input) {
-            Ok(("", _expr)) =>{
+            Ok(("", _expr)) => {
                 // Evaluate the expression
                 output = repl_parse_expression(input, &current_env);
-            },
+            }
             Ok((_, _)) => {
                 // Try to parse statements in the input
-                match repl_parse_statements(input, current_env.clone()){
+                match repl_parse_statements(input, current_env.clone()) {
                     Ok(new_env) => current_env = new_env,
                     Err(e) => output = Err(e),
                 };
-            },
+            }
             Err(e) => output = Err(e.to_string()),
-            
         }
-        match output{
+        match output {
             // Prints the output -> if no output -> continue the loop
             Ok(result) => {
                 if !result.is_empty() {
                     println!("{}", result);
-                }
-                else {
-                    continue
+                } else {
+                    continue;
                 }
             }
             Err(e) => println!("Sintax Error: {}", e),
@@ -77,43 +75,50 @@ pub fn repl() -> io::Result<()> {
     Ok(())
 }
 
-fn repl_parse_expression(input: &str, current_env: &HashMap<String, Expression>) -> Result<String, String>{
+fn repl_parse_expression(
+    input: &str,
+    current_env: &HashMap<String, Expression>,
+) -> Result<String, String> {
     // Parse the input as an expression
     match expression(input) {
-        Ok(("", expr)) =>{
+        Ok(("", expr)) => {
             // Evaluate the expression
             match eval(expr, &current_env.clone()) {
-                Ok(evaluated_expression) => {
-                    match evaluated_expression{
-                        Expression::CInt(val) => Ok(val.to_string()),
-                        Expression::CReal(val) => Ok(val.to_string()),
-                        Expression::CString(string) => Ok(string),
-                        Expression::CTrue => Ok(String::from("True")),
-                        Expression::CFalse => Ok(String::from("False")),
-                        _ => Err(format!("NonExistent Type")),
-                    }
+                Ok(evaluated_expression) => match evaluated_expression {
+                    Expression::CInt(val) => Ok(val.to_string()),
+                    Expression::CReal(val) => Ok(val.to_string()),
+                    Expression::CString(string) => Ok(string),
+                    Expression::CTrue => Ok(String::from("True")),
+                    Expression::CFalse => Ok(String::from("False")),
+                    _ => Err(format!("NonExistent Type")),
                 },
                 Err(e) => Err(format!("Evaluation Error: {}", e)),
             }
-        },
+        }
         Ok((_, _)) => Err(format!("Parsing Expression Error")),
-        Err(_)=> Err(format!("Parsing Expression Error"))
+        Err(_) => Err(format!("Parsing Expression Error")),
     }
 }
 
-fn repl_parse_statements(input: &str, mut current_env: HashMap<String, Expression>) -> Result<HashMap<String, Expression>, String> {
+fn repl_parse_statements(
+    input: &str,
+    mut current_env: HashMap<String, Expression>,
+) -> Result<HashMap<String, Expression>, String> {
     // Parse the input as a statement
     match parse(input) {
         Ok((remaining, statements)) => {
             if !remaining.is_empty() {
-                return Err(format!("Warning: Unparsed input remains: {:?}\n", remaining));
+                return Err(format!(
+                    "Warning: Unparsed input remains: {:?}\n",
+                    remaining
+                ));
             }
 
             for stmt in statements {
                 match execute(stmt, current_env.clone()) {
                     Ok(new_env) => current_env = new_env,
                     Err(e) => return Err(format!("Execution Error: {}", e)),
-                }    
+                }
             }
             Ok(current_env.clone())
         }
@@ -121,7 +126,9 @@ fn repl_parse_statements(input: &str, mut current_env: HashMap<String, Expressio
     }
 }
 
-mod tests{
+mod tests {
+
+    use super::*;
 
     #[test]
     fn test_simple_repl_parse_expression1() {
@@ -227,7 +234,6 @@ mod tests{
         }
     }
 
-
     #[test]
     fn test_typechecker_sad_path_repl_parse_expression() {
         let input = "a + b";
@@ -237,7 +243,10 @@ mod tests{
         let output = repl_parse_expression(input, &env);
         match output {
             Ok(_) => panic!("Error was expected"),
-            Err(e) => assert_eq!("Evaluation Error: addition '(+)' is only defined for numbers (integers and real).", e),
+            Err(e) => assert_eq!(
+                "Evaluation Error: addition '(+)' is only defined for numbers (integers and real).",
+                e
+            ),
         }
     }
 
@@ -308,7 +317,6 @@ mod tests{
 
     #[test]
     fn test_complex_repl_parse_assigment1() {
-
         // R-Python >> a = 10
         // R-Python >> b = a
 
@@ -316,7 +324,7 @@ mod tests{
         let mut env = HashMap::new();
         let mut env_expected = HashMap::new();
         env_expected.insert(String::from("a"), Expression::CInt(10));
-        match repl_parse_statements(input, env){
+        match repl_parse_statements(input, env) {
             Ok(new_env) => {
                 assert_eq!(new_env, env_expected);
                 env = new_env;
@@ -336,7 +344,6 @@ mod tests{
 
     #[test]
     fn test_complex_repl_parse_assigment2() {
-
         // R-Python >> a = 10
         // R-Python >> b = a
 
@@ -344,7 +351,7 @@ mod tests{
         let mut env = HashMap::new();
         let mut env_expected = HashMap::new();
         env_expected.insert(String::from("a"), Expression::CInt(10));
-        match repl_parse_statements(input, env){
+        match repl_parse_statements(input, env) {
             Ok(new_env) => {
                 assert_eq!(new_env, env_expected);
                 env = new_env;
@@ -364,7 +371,6 @@ mod tests{
 
     #[test]
     fn test_complex_repl_parse_assigment2() {
-
         // R-Python >> a = 10
         // R-Python >> b = a
 
@@ -372,7 +378,7 @@ mod tests{
         let mut env = HashMap::new();
         let mut env_expected = HashMap::new();
         env_expected.insert(String::from("a"), Expression::CInt(10));
-        match repl_parse_statements(input, env){
+        match repl_parse_statements(input, env) {
             Ok(new_env) => {
                 assert_eq!(new_env, env_expected);
                 env = new_env;
@@ -390,4 +396,3 @@ mod tests{
         }
     }
 }
-
